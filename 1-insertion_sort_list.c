a89c83235e6af7eadd78a8dc06c06028d67ffc02
MarwaAbuEssa <meromeroo@gmail.com>
2023-10-24 00:12:19 +0300

    insert sort and BigO file

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,23 +2,23 @@
 
 /**
  * swap_nodes - Swap two nodes in doubly-linked list.
- * @h:  head 
- * @n1: first node to swap.
- * @n2: second node to swap.
+ * @header:  head 
+ * @node1: first node to swap.
+ * @node2: second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_nodes(listint_t **header, listint_t **node1, listint_t *node2)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*header = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
 
 /**
@@ -28,18 +28,18 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *inode, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (inode = (*list)->next; inode != NULL; inode = tmp)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
-		while (insert != NULL && iter->n < insert->n)
+		tmp = inode->next;
+		insert = inode->prev;
+		while (insert != NULL && inode->n < insert->n)
 		{
-			swap_nodes(list, &insert, iter);
+			swap_nodes(list, &insert, inode);
 			print_list((const listint_t *)*list);
 		}
 	}
