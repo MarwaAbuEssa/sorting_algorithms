1015533970067d7a93c58e263e4d235ed7be1e41
MarwaAbuEssa <meromeroo@gmail.com>
2023-10-23 23:48:25 +0300

    insert sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,11 +1,10 @@
-
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * swap_nodes - Swap two nodes in doubly-linked list.
+ * @h:  head 
+ * @n1: first node to swap.
+ * @n2: second node to swap.
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
@@ -23,11 +22,9 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
- * @list: A pointer to the head of a doubly-linked list of integers.
- *
- * Description: Prints the list after each swap.
+ * insertion_sort_list - insertion sort a doubly linked list
+ * @list: pointer to the head .
+ * Description: Prints list .
  */
 void insertion_sort_list(listint_t **list)
 {
